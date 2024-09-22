7550b25cb634abc0bfa337b97a22d13c7e000f77
Bishnu Mishra <mishravishnu369@gmail.com>
2024-09-23 03:38:49 +0530

    added modal for food images when clicked, it becomes big

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -1,12 +1,19 @@
 import React, {useState} from 'react'
+import {Modal} from 'react-bootstrap'
 
 export default function Pizza({pizza}) {
     const [quantity , setquantity] = useState(1);
     const [varient , setvarient] = useState('small');
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
   return (
     <div style={{margin: '69px'}} className='shadow-lg p-3 mb-5 bg-white rounded'>
-        <h1>{pizza.name}</h1>
-        <img src={pizza.image} alt="pizza-image" style={{height:'200px', width:'200px'}}/>
+        <div onClick={handleShow}>
+            <h1>{pizza.name}</h1>
+            <img src={pizza.image} alt="pizza-image" style={{height:'200px', width:'200px'}}/>
+        </div>
+
         <div className="flex-container">
             <div className='w-100 m-1'>
                 <p>Varients</p>
@@ -34,8 +41,20 @@ export default function Pizza({pizza}) {
             <div className='m-1 w-100'>
                 <button className="btn">ADD TO CART</button>
             </div>
-
         </div>
+
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>{pizza.name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <img src={pizza.image} alt="pizza-image" className='img-fluid' style={{height: '400px'}}/>
+                <p>{pizza.description}</p>
+            </Modal.Body>
+            <Modal.Footer>
+                <button className='btn' onClick={handleClose}>CLOSE</button>
+            </Modal.Footer>
+        </Modal>
     </div>
   )
 }
