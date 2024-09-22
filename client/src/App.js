ff4a56382a2ec70b9c6dc632acf1d55e575b6e14
Bishnu Mishra <mishravishnu369@gmail.com>
2024-09-23 02:10:46 +0530

    created components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Homescreen from './screens/Homescreen';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Homescreen/>
     </div>
   );
 }
