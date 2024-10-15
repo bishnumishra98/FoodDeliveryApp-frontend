428a5af968b2d9b6e95266cb6324905df2229bc9
Bishnu Mishra <mishravishnu369@gmail.com>
2024-10-16 03:33:10 +0530

    reconfigured store properly, fixed some bugs

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,37 @@
-import {combineReducers} from 'redux'
-import {createStore, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'reux-devtools-extension'
-import { getAllPizzasReducer } from './reducers/pizzaReducers'
+// import {combineReducers} from 'redux'
+// import {createStore, applyMiddleware} from 'redux'
+// import thunk from 'redux-thunk'
+// import {composeWithDevTools} from '@redux-devtools/extension'
+// import { getAllPizzasReducer } from './reducers/pizzaReducers'
+
+// const finalReducer = combineReducers({
+//     getAllPizzasReducer: getAllPizzasReducer
+// })
+// const initialState = {}
+
+// const composeEnhancers = composeWithDevTools({})
+
+// const store = createStore(finalReducer, initialState, composeEnhancers(applyMiddleware(thunk)))
+
+// export default store
+
+// -----------------------------------------------------------------------------------
+// createStore is depreciated. So tha above code will not work:
+// -----------------------------------------------------------------------------------
+
+import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { thunk } from 'redux-thunk';
+import { getAllPizzasReducer } from './reducers/pizzaReducers';
 
 const finalReducer = combineReducers({
-    getAllPizzasReducer: getAllPizzasReducer
-})
-const initialState = {}
+    getAllPizzasReducer: getAllPizzasReducer,
+});
 
-const composeEnhancers = composeWithDevTools({})
+const store = configureStore({
+    reducer: finalReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+    devTools: process.env.NODE_ENV !== 'production',
+});
 
-const store = createStore(finalReducer, initialState, composeEnhancers(applyMiddleware(thunk)))
-
-export default store
+export default store;
