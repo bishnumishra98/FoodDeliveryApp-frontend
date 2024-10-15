ae5f944b36527ea364dc39fb6831e897e4a969c4
Bishnu Mishra <mishravishnu369@gmail.com>
2024-10-16 04:40:16 +0530

    modified Homescreen.js to display Loading icon and then display pizzas from backend server. Removed dependenxy on local pizzadata.

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import pizzas from '../pizzadata'
 import Pizza from '../components/Pizza'
 import { getAllPizzas } from '../actions/pizzaActions';
 
 export default function Homescreen() {
     const dispatch = useDispatch();
+    const pizzasstate = useSelector(state => state.getAllPizzasReducer)
+    const {pizzas, error, loading} = pizzasstate
 
     useEffect(() => {
         dispatch(getAllPizzas())
@@ -14,13 +15,17 @@ export default function Homescreen() {
     return (
         <div>
             <div className="row">
-                {pizzas.map(pizza => {
-                    return <div className="col-md-4">
-                        <div>
-                            <Pizza pizza = {pizza}/>
+                {loading ? (<h1>Loading...</h1>) : error ? (<h1>Something went wrong</h1>) : (
+                    pizzas.map(pizza => {
+                        return <div className="col-md-4">
+                            <div>
+                                <Pizza pizza = {pizza}/>
+                            </div>
                         </div>
-                    </div>
-                })}
+                    })
+                )}
+
+                
             </div>
         </div>
     )
