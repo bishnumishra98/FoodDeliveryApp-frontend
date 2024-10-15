ebd005ca44a17b13ae135dffe9a6d1fc9ef06855
Bishnu Mishra <mishravishnu369@gmail.com>
2024-10-16 05:16:17 +0530

    fixed the bug of failed to load resource -> http://localhost:3000/api/pizzas/getallpizzas

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,31 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import pizzas from '../pizzadata'
 import Pizza from '../components/Pizza'
 import { getAllPizzas } from '../actions/pizzaActions';
 
 export default function Homescreen() {
     const dispatch = useDispatch();
-    const pizzasstate = useSelector((state) => state.getAllPizzasReducer)
-    const {pizzas, error, loading} = pizzasstate;
 
     useEffect(() => {
         dispatch(getAllPizzas())
-    }, []);
+    }, [])
 
     return (
         <div>
             <div className="row">
-                {loading ? (<h1>Loading...</h1>) : error ? (<h1>Something went wrong</h1>) : (
-                    pizzas.map(pizza => {
-                        return <div className="col-md-4">
-                            <div>
-                                <Pizza pizza = {pizza}/>
-                            </div>
+                {pizzas.map(pizza => {
+                    return <div className="col-md-4">
+                        <div>
+                            <Pizza pizza = {pizza}/>
                         </div>
-                    })
-                )}
-
-                
+                    </div>
+                })}
             </div>
         </div>
     )
