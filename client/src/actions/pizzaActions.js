05dd85f863a85f81ec872fac9354805376d0db67
Bishnu Mishra <mishravishnu369@gmail.com>
2024-10-16 19:29:15 +0530

    added a semicolon, nothing much. All set and perfect till now.

--- client/src/actions/pizzaActions.js
+++ client/src/actions/pizzaActions.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const getAllPizzas = () => async dispatch => {
+export const getAllPizzas = () => async (dispatch) => {
 
-    dispatch({type:'GET_PIZZAS_REQUEST'})
+    dispatch({type:'GET_PIZZAS_REQUEST'});
 
     try {
         const response = await axios.get('/api/pizzas/getallpizzas')
