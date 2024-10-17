574c58fe657a134294b0173aea07af92592bf8d9
Bishnu Mishra <mishravishnu369@gmail.com>
2024-10-17 23:09:39 +0530

    added if else condition to cartActions.js to keep item in range of 1 to 10

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -11,6 +11,8 @@ export const addToCart = (pizza, quantity, variant) => (dispatch, getState) => {
 
     if(cartItem.quantity > 10) {
         alert("Please contact the restaurant owner for ordering more than 10 quantity of this item.\nOwner contact: +91 9330188837");
+    } else if(cartItem.quantity < 1) {
+        dispatch({type: 'DELETE_FROM_CART', payload: pizza});
     } else {
         dispatch({type: 'ADD_TO_CART', payload: cartItem});
     }
