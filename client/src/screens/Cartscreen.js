b8a54251a600376ffab2a8b32c3df2223988d882
Bishnu Mishra <mishravishnu369@gmail.com>
2024-10-17 23:54:57 +0530

    added Total functionality in cart screen

--- client/src/screens/Cartscreen.js
+++ client/src/screens/Cartscreen.js
@@ -6,6 +6,7 @@ import { deleteFromCart } from '../actions/cartActions';
 export default function Cartscreen() {
     const cartstate = useSelector(state => state.cartReducer);
     const cartItems = cartstate.cartItems;
+    var total = cartItems.reduce((x, item) => x + item.price*item.quantity, 0);
     const dispatch = useDispatch();
 
     return (
@@ -17,7 +18,7 @@ export default function Cartscreen() {
                         return <div className="flex-container">
                             <div className='text-start m-1 w-100'>
                                 <h1>{item.name} [{item.variant}]</h1>
-                                <h1>Price: {item.quantity} * {item.price} = {item.quantity * item.price}</h1>
+                                <h1>Price: {item.quantity} &times; ₹{item.price} = ₹{item.quantity * item.price}</h1>
                                 <h1 style={{display: 'inline'}}>Quantity: </h1>
                                 <i className="fa-solid fa-plus" onClick={() => {dispatch(addToCart(item, item.quantity+1, item.variant))}}></i>
                                 <b>{item.quantity}</b>
@@ -34,9 +35,14 @@ export default function Cartscreen() {
                     })}
                 </div>
                 
-                <div className="col-md-4">
+                {/* Only show this block, when something is added in the cart, i.e., total > 0 */}
+                {total > 0 && (
+                    <div className="col-md-4 text-end">
+                        <h2 style={{fontSize: '45px'}}>Total: ₹{total}</h2>
+                        <button className='btn' style={{fontSize: '20px'}}>Place order</button>
+                    </div>
+                )}
 
-                </div>
             </div>
         </div>
     )
