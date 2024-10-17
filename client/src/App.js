dfd98e836d948533908eff4f5a67412b2f294ab8
Bishnu Mishra <mishravishnu369@gmail.com>
2024-10-17 16:25:02 +0530

    added basic Cartscreen

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Homescreen from './screens/Homescreen';
+import { BrowserRouter, Routes, Route, Link, Switch } from 'react-router-dom';
+import Cartscreen from './screens/Cartscreen';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Homescreen/>
+      <Navbar />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Homescreen />} />
+				<Route path="/cart" element={<Cartscreen />} />
+			</Routes>
+		</BrowserRouter>
     </div>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
 	return (
 		<nav className="navbar navbar-expand-lg bg-body-tertiary shadow-lg p-3 mb-5 bg-white rounded">
 			<div className="container-fluid">
-				<a className="navbar-brand" href="#">Food Delivery App</a>
+				<a className="navbar-brand" href="/">Food Delivery App</a>
 					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 						<span className="navbar-toggler-icon"></span>
 					</button>
@@ -19,7 +19,7 @@ const Navbar = () => {
 						<a className="nav-link active" aria-current="page" href="#">Login</a>
 					</li>
 					<li className="nav-item">
-						<a className="nav-link" href="#">Cart {cartstate.cartItems.length}</a>
+						<a className="nav-link" href="/cart">Cart {cartstate.cartItems.length}</a>
 					</li>
 					</ul>
 				</div>
