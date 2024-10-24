c5b858a38118073dcec5b978314d42e47334dec0
Bishnu Mishra <mishravishnu369@gmail.com>
2024-10-25 03:00:53 +0530

    completed authentication process

--- client/src/screens/Loginscreen.js
+++ client/src/screens/Loginscreen.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import {useDispatch , useSelector} from 'react-redux'
 import { loginUser } from "../actions/userActions";
+import Loading from "../components/Loading";
+import Error from "../components/Error";
 
 export default function Loginscreen() {
 	const [email, setemail] = useState("");
     const [password, setpassword] = useState("");
+	const loginstate = useSelector(state => state.loginUserReducer);
+	const {loading, error} = loginstate;
+
     const dispatch = useDispatch();
 
 	useEffect(() => {
@@ -24,6 +29,8 @@ export default function Loginscreen() {
         	<div className="row justify-content-center mt-5">
 				<div className="col-md-5 mt-5 text-left shadow-lg p-3 mb-5 bg-white rounded">
 					<h2 className="text-center m-2" style={{ fontSize: "35px" }}> Login </h2>
+					{loading && (<Loading/>)}
+					{error && (<Error error = 'Invalid credentials'/>)}
 					<div>
 						<input required type="text" placeholder="email" className="form-control" value={email} onChange={(e)=>{setemail(e.target.value)}} />
 						<input type="text" placeholder="password" className="form-control" value={password} required onChange={(e)=>{setpassword(e.target.value)}} />
--- client/src/screens/Registerscreen.js
+++ client/src/screens/Registerscreen.js
@@ -1,12 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { registerUser } from '../actions/userActions';
+import Loading from '../components/Loading';
+import Success from '../components/Success';
+import Error from '../components/Error';
 
 export default function Registerscreen() {
 	const [name, setname] = useState('');
 	const [email, setemail] = useState('');
 	const [password, setpassword] = useState('');
 	const [cpassword, setcpassword] = useState('');
+	const registerstate = useSelector(state => state.registerUserReducer);
+	const {error, loading, success} = registerstate;
 
 	const dispatch = useDispatch();
 
@@ -28,6 +33,9 @@ export default function Registerscreen() {
 		<div>
 			<div className="row justify-content-center mt-5">
 				<div className="col-md-5 mt-5 text-start shadow-lg p-3 mb-5 bg-white rounded">
+					{loading && (<Loading/>)}
+					{success && (<Success success = 'User registered successfully. Please login now.'/>)}
+					{error && (<Error error = 'This email is already registered'/>)}
 					<h2 className='text-center m-3' style={{fontSize: '33px'}}>Register</h2>
 					<div>
 						<input required type="text" placeholder='Name' className='form-control' value={name} onChange={(e) => {setname(e.target.value)}} />
