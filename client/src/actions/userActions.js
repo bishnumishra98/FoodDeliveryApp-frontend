d32e26589e69bce17add64e52a23b02251a16941
Bishnu Mishra <mishravishnu369@gmail.com>
2024-10-24 22:03:20 +0530

    added loginUserReducer function in userReducers.js

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -16,12 +16,10 @@ export const loginUser=(user)=>async dispatch=>{
     dispatch({type:'USER_LOGIN_REQUEST'});
 
     try {
-        const response = await axios.post('/api/users/login' , user)
+        const response = await axios.post('/api/users/login' , user);
         console.log(response);
-        dispatch({type:'USER_LOGIN_SUCCESS' , payload: response.data})
-        localStorage.setItem('currentUser' , JSON.stringify(response.data))
-        window.location.href='/'
+        dispatch({type:'USER_LOGIN_SUCCESS' , payload: response.data});
     } catch (error) {
-        dispatch({type:'USER_LOGIN_FAILED' , payload: error})
+        dispatch({type:'USER_LOGIN_FAILED' , payload: error});
     }
 }
