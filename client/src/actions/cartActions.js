6c3d370ba6d4ae8c43fcb68355f204552f4b7457
Bishnu Mishra <mishravishnu369@gmail.com>
2024-11-26 10:13:29 +0530

    removed if(quantity > 10, contact owner) from cartActions.js

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -8,9 +8,7 @@ export const addToCart = (food, quantity) => (dispatch, getState) => {
 		price: food.price,
 	};
 
-	if (cartItem.quantity > 10) {
-		alert("Please contact the restaurant owner for ordering more than 10 quantity of this item.\nOwner contact: +91 9330188837");
-	} else if (cartItem.quantity < 1) {
+	if (cartItem.quantity < 1) {
 		dispatch({ type: "DELETE_FROM_CART", payload: food });
 	} else {
 		dispatch({ type: "ADD_TO_CART", payload: cartItem });
