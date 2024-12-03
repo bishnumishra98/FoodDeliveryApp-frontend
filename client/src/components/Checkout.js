71f8411125281212960a06f8a1f5d68997738afe
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-04 01:16:59 +0530

    nm

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import {useDispatch , useSelector} from 'react-redux'
-import StripeCheckout from 'react-stripe-checkout'
+// import StripeCheckout from 'react-stripe-checkout'
 import { placeOrder } from '../actions/orderActions'
 import Error from "../components/Error";
 import Loading from "../components/Loading";
@@ -25,18 +25,17 @@ export default function Checkout({subtotal}) {
             {error && (<Error error='Something went wrong'/>)}
             {success && (<Success success='Your Order Placed Successfully'/>)}
 
-            <StripeCheckout
+            {/* Uncomment it while implementing Razorpay */}
+            {/* <StripeCheckout
             amount={subtotal*100}
             shippingAddress
             token={tokenHander}
             stripeKey='pk_test_51IYnC0SIR2AbPxU0TMStZwFUoaDZle9yXVygpVIzg36LdpO8aSG8B9j2C0AikiQw2YyCI8n4faFYQI5uG3Nk5EGQ00lCfjXYvZ'
             currency='INR'
             >
-                <button className='btn'>Pay Now</button>
-            </StripeCheckout>
+                <button className='btn'>Place Order</button>
+            </StripeCheckout> */}
             
         </div>
     )
 }
-
-
