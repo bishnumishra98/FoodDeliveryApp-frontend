f0eb1c53be20b3255622423a2cbac2164b98eef1
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-10 13:29:44 +0530

    nm

--- src/reducers/orderReducers.js
+++ src/reducers/orderReducers.js
@@ -1,15 +1,15 @@
 export const placeOrderReducer =(state={} , action) =>{
     switch(action.type) {
         case 'PLACE_ORDER_REQUEST' : return{
-            loading:true
+            loading : true
         }
         case 'PLACE_ORDER_SUCCESS' : return{
-            loading:false,
-            success:true
+            loading : false,
+            success : true
         }
         case 'PLACE_ORDER_FAILED' : return{
-            loading:false,
-            error:action.payload
+            loading : false,
+            error : action.payload
         }
         default : return state
     }
