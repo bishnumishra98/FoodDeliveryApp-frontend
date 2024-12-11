9c8ff0d1b407b41ad062901d5d498e635150bd95
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-11 20:47:34 +0530

    nm

--- src/screens/Ordersscreen.js
+++ src/screens/Ordersscreen.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Ordersscreen() {
     return (
         <div>
-            <h2 style={{fontSize: "25px"}}>My orders</h2>
+            <h2 style={{fontSize: "25px"}}>My orders page is under development...</h2>
         </div>
     )
 }
