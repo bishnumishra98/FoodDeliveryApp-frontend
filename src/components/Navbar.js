2e26b8711a42fccbf9a090a9c2a585c6ec82c002
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-13 04:11:21 +0530

    modified cart UI in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,65 +1,116 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import { useSelector, useDispatch } from "react-redux";
 import { logoutUser } from "../actions/foodActions";
 
 const Navbar = () => {
-	const cartstate = useSelector((state) => state.cartReducer);
-	const userstate = useSelector((state) => state.loginUserReducer);
-	console.log(userstate);
-	const { currentUser } = userstate;
-	const dispatch = useDispatch();
+    const cartstate = useSelector((state) => state.cartReducer);
+    const userstate = useSelector((state) => state.loginUserReducer);
+    const { currentUser } = userstate;
+    const dispatch = useDispatch();
 
-	return (
-		<nav className="navbar navbar-expand-lg bg-body-tertiary shadow-lg p-2 mb-5 bg-white rounded" style={{ position: "sticky", top: 0, zIndex: 1000 }}>
-			<div className="container-fluid">
-				<a className="navbar-brand" href="/">Food Delivery App</a>
-				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarNav">
-					{/* Add 'ms-auto' class to align nav items to the left */}
-					<ul className="navbar-nav ms-auto">
-						{currentUser ? (
-						<div className="dropdown mt-2">
-							<a
-							className="dropdown-toggle"
-							style={{ color: "black", textDecoration: "none" }}
-							type="button"
-							id="dropdownMenuButton"
-							data-bs-toggle="dropdown"
-							aria-haspopup="true"
-							aria-expanded="false"
-							>
-								{currentUser.name.split(" ")[0]}
-							</a>
-							<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-								<a className="dropdown-item" href="/orders">My orders</a>
-								<a className="dropdown-item" href="#" onClick={() => {dispatch(logoutUser());}}><li>Logout</li></a>
-							</div>
-						</div>
-						) : (
-						<li className="nav-item">
-							<a
-							className="nav-link active"
-							aria-current="page"
-							href="/login"
-							>
-							Login
-							</a>
-						</li>
-						)}
+    return (
+        <nav
+            className="navbar navbar-expand-lg bg-body-tertiary shadow-lg p-2 mb-5 bg-white rounded"
+            style={{ position: "sticky", top: 0, zIndex: 1000 }}
+        >
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">
+                    Food Delivery App
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        {currentUser ? (
+                            <div className="dropdown mt-2">
+                                <a
+                                    className="dropdown-toggle"
+                                    style={{
+                                        color: "black",
+                                        textDecoration: "none",
+                                    }}
+                                    type="button"
+                                    id="dropdownMenuButton"
+                                    data-bs-toggle="dropdown"
+                                    aria-haspopup="true"
+                                    aria-expanded="false"
+                                >
+                                    {currentUser.name.split(" ")[0]}
+                                </a>
+                                <div
+                                    className="dropdown-menu"
+                                    aria-labelledby="dropdownMenuButton"
+                                >
+                                    <a className="dropdown-item" href="/orders">
+                                        My orders
+                                    </a>
+                                    <a
+                                        className="dropdown-item"
+                                        href="#"
+                                        onClick={() => {
+                                            dispatch(logoutUser());
+                                        }}
+                                    >
+                                        <li>Logout</li>
+                                    </a>
+                                </div>
+                            </div>
+                        ) : (
+                            <li className="nav-item">
+                                <a
+                                    className="nav-link active"
+                                    aria-current="page"
+                                    href="/login"
+                                >
+                                    Login
+                                </a>
+                            </li>
+                        )}
 
-						<li className="nav-item">
-							<a className="nav-link" href="/cart">
-								Cart {cartstate.cartItems.length}
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	);
+                        <li className="nav-item">
+                            <a
+                                className="nav-link"
+                                href="/cart"
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                }}
+                            >
+                                <i
+                                    className="fas fa-shopping-cart"
+                                    style={{ fontSize: "20px", marginRight: "8px" }}
+                                ></i>
+                                {cartstate.cartItems.length > 0 && (
+                                    <span
+                                        style={{
+                                            background: "red",
+                                            color: "white",
+                                            borderRadius: "50%",
+                                            padding: "3px 8px",
+                                            fontSize: "12px",
+                                        }}
+                                    >
+                                        {cartstate.cartItems.length}
+                                    </span>
+                                )}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
