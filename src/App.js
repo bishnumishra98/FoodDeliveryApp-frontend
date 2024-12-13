b4dcc406e272ae513c018526c4d9bb15f9361548
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-13 18:45:19 +0530

    removed unnecessary imports in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Homescreen from './screens/Homescreen';
 import 'bootstrap';
-import { BrowserRouter, Routes, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cartscreen from './screens/Cartscreen';
 import Registerscreen from './screens/Registerscreen';
 import Loginscreen from './screens/Loginscreen';
@@ -10,24 +10,23 @@ import Ordersscreen from './screens/Ordersscreen';
 import Failedpaymentscreen from './screens/Failedpaymentscreen';
 import Adminscreen from './screens/Adminscreen';
 
-
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Homescreen />} />
-				<Route path="/cart" element={<Cartscreen />} />
-				<Route path="/register" element={<Registerscreen />} />
-				<Route path="/login" element={<Loginscreen />} />
-				<Route path="/orders" element={<Ordersscreen />} />
-				<Route path="/failedpayment" element={<Failedpaymentscreen />} />
-				<Route path="/admin" element={<Adminscreen />} />
-			</Routes>
-		</BrowserRouter>
-    </div>
-  );
+	return (
+		<div className="App">
+		<Navbar />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Homescreen />} />
+					<Route path="/cart" element={<Cartscreen />} />
+					<Route path="/register" element={<Registerscreen />} />
+					<Route path="/login" element={<Loginscreen />} />
+					<Route path="/orders" element={<Ordersscreen />} />
+					<Route path="/failedpayment" element={<Failedpaymentscreen />} />
+					<Route path="/admin" element={<Adminscreen />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
