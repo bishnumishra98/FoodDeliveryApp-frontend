473b9b544db30bf32ce73d65f11fe9e004b401ce
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-14 01:08:18 +0530

    removed case PLACE_ORDER_SUCCESS from orderReducers.js

--- src/reducers/orderReducers.js
+++ src/reducers/orderReducers.js
@@ -3,10 +3,6 @@ export const placeOrderReducer = (state={} , action) => {
         case 'PLACE_ORDER_REQUEST' : return {
             loading : true
         }
-        case 'PLACE_ORDER_SUCCESS' : return {
-            loading : false,
-            success : true
-        }
         case 'PLACE_ORDER_FAILED' : return {
             loading : false,
             error : action.payload
