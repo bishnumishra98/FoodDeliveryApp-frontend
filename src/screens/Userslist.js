21c08ac0bc5c64cf081fcfc93a17123603eb2211
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-17 11:13:07 +0530

    removed authority column from Userslist.js

--- src/screens/Userslist.js
+++ src/screens/Userslist.js
@@ -37,7 +37,7 @@ export default function Userslist() {
                         <th>User ID</th>
                         <th>Name</th>
                         <th>Email</th>
-                        <th>Authority</th>
+                        {/* <th>Authority</th> */}
                         <th>Delete</th>
                     </tr>
                 </thead>
@@ -50,8 +50,7 @@ export default function Userslist() {
                                     <td>{user._id}</td>
                                     <td>{user.name}</td>
                                     <td>{user.email}</td>
-                                    <td>
-                                        {/* Dropdown to change authority */}
+                                    {/* <td>
                                         <select
                                             className="form-control"
                                             value={user.isAdmin ? "Admin" : "Non-Admin"}
@@ -60,7 +59,7 @@ export default function Userslist() {
                                             <option value="Admin">Admin</option>
                                             <option value="Non-Admin">Non-Admin</option>
                                         </select>
-                                    </td>
+                                    </td> */}
                                     <td>
                                         <i
                                             className="fa fa-trash trash-icon"
